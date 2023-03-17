90d21a0eb54733e98df95f5b45534b7f97de431a
rlramhap <rlramhap@buffalo.edu>
2023-03-17 17:04:16 -0400

    create table in schedule page #31
    instantiates schedule grid M-F 8a-5p, with some fake events

--- mySchedule.php
+++ mySchedule.php
@@ -1,10 +1,115 @@
-<!DOCTYPE HTML>  
+<!DOCTYPE HTML>
 <html>
-    <head>
-        My Schedules Page
-    </head>
-<body>  
-    <h1>My Schedules<h1>
-    <h2> nothing 2 see here :) <h2>
-    
+
+<head>
+	The My Schedules Page
+	<style>
+		table,
+		th,
+		td {
+			border: 1px solid black;
+			border-collapse: collapse;
+		}
+
+		td:nth-child(even),
+		th:nth-child(even) {
+			background-color: #ffcccc;
+		}
+	</style>
+</head>
+
+<body>
+	<h1>My Schedule<h1>
+			<table>
+				<tr>
+					<th>Time</th>
+					<th>Monday</th>
+					<th>Tuesday</th>
+					<th>Wednesday</th>
+					<th>Thursday</th>
+					<th>Friday</th>
+				</tr>
+				<tr>
+					<th>8am</th>
+					<th> </th>
+					<th> </th>
+					<th> </th>
+					<th> </th>
+					<th> </th>
+				</tr>
+				<tr>
+					<th>9am</th>
+					<th>CSE 115 Recitation</th>
+					<th> </th>
+					<th>CSE 115 Recitation</th>
+					<th> </th>
+					<th>CSE 115 Recitation</th>
+				</tr>
+				<tr>
+					<th>10am</th>
+					<th> CSE 115 Recitation</th>
+					<th> </th>
+					<th> CSE 115 Recitation</th>
+					<th> </th>
+					<th> CSE 115 Recitation</th>
+				</tr>
+				<tr>
+					<th>11am</th>
+					<th> </th>
+					<th> </th>
+					<th> </th>
+					<th> </th>
+					<th> </th>
+				</tr>
+				<tr>
+					<th>12pm</th>
+					<th> </th>
+					<th> </th>
+					<th> </th>
+					<th> </th>
+					<th> </th>
+				</tr>
+				<tr>
+					<th>1pm</th>
+					<th> </th>
+					<th> CSE 220 Lab</th>
+					<th> </th>
+					<th> CSE 220 Lab</th>
+					<th> </th>
+				</tr>
+				<tr>
+					<th>2pm</th>
+					<th> </th>
+					<th> </th>
+					<th> </th>
+					<th> </th>
+					<th> </th>
+				</tr>
+				<tr>
+					<th>3pm</th>
+					<th>CSE 312 Class </th>
+					<th> </th>
+					<th>CSE 312 Class </th>
+					<th> </th>
+					<th>CSE 312 Class </th>
+				</tr>
+				<tr>
+					<th>4pm</th>
+					<th> </th>
+					<th> </th>
+					<th> </th>
+					<th> </th>
+					<th> </th>
+				</tr>
+				<tr>
+					<th>5pm</th>
+					<th> </th>
+					<th> </th>
+					<th> </th>
+					<th> </th>
+					<th> </th>
+				</tr>
+			</table>
 </body>
+
+</html>
