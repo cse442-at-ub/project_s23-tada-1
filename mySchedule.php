0d6054bd1b502b9affc63b80a8b4d242348ab6d5
ahanabhattchrya <ahanabha@buffalo.edu>
2023-03-17 17:40:43 -0400

    Added Edit Schedule Button and Class and Event Type Dropdown to My Schedule #31
    These buttons are to edit a user's schedule and add a type of event to the table

--- mySchedule.php
+++ mySchedule.php
@@ -5,6 +5,20 @@
     </head>
 <body>  
     <h1>My Schedules<h1>
+    <button type="button">Edit Schedule</button>
+    <h4> Filters:<h4>
+    <select name="Class" id="class">
+        <option value="cse101">Class</option>
+        <option value="cse101">CSE 101</option>
+        <option value="cse115">CSE 115</option>
+        <option value="cse220">CSE 220</option>
+        <option value="cse250">CSE 250</option>
+    </select>   
+    <select name="EventType" id="event-type">
+        <option value="lecture">Event Type</option>
+        <option value="lecture">Lecture</option>
+        <option value="office-hours">Office Hours</option>
+        <option value="recitation">Recitation</option>
+    </select>  
     <h2> nothing 2 see here :) <h2>
-    
 </body>
