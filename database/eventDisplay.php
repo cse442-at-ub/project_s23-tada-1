349119915bb28e3d1b064b2fd11d80853d9c62f1
Kelly Lam <khlam@buffalo.edu>
2023-04-05 22:51:33 -0400

    Created array_sort function #39
    Created the array_sort helper function to sort the day lists by the Time key.

--- database/eventDisplay.php
+++ database/eventDisplay.php
@@ -11,6 +11,45 @@
 //----------It'll return a list of lists (where Index 1 = Monday, Index 2 = Tuesday, etc.---------------------------------------------#
 //----------Note: You must access this structure/class because you'll need to be able to read this on the mySchedule page later.------#
 
+// Helper function to sort array by key //
+
+// Function taken from https://www.php.net/manual/en/function.sort.php //
+function array_sort($array, $on, $order=SORT_ASC)
+{
+    $new_array = array();
+    $sortable_array = array();
+
+    if (count($array) > 0) {
+        foreach ($array as $k => $v) {
+            if (is_array($v)) {
+                foreach ($v as $k2 => $v2) {
+                    if ($k2 == $on) {
+                        $sortable_array[$k] = $v2;
+                    }
+                }
+            } else {
+                $sortable_array[$k] = $v;
+            }
+        }
+
+        switch ($order) {
+            case SORT_ASC:
+                asort($sortable_array);
+            break;
+            case SORT_DESC:
+                arsort($sortable_array);
+            break;
+        }
+
+        foreach ($sortable_array as $k => $v) {
+            $new_array[$k] = $array[$k];
+        }
+    }
+
+    return $new_array;
+}
+
+
 function listDisplayEvents($username)
 {
     include('./backend/connection.php');
