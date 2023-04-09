d8b3afcbc1aa1f25b1950d126f567120897f79ad
Tim <tkong@buffalo.edu>
2023-04-08 23:23:04 -0400

    implemented basic job application functionality #41

---
+++ jobApplication.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head></head>
+    <body>
+        <form method="GET" action="index.php">
+            Name: <input type="text" name="application_name">
+            <br>
+            <br>
+            Experience: <input type="text" name="application_experience">
+            <br>
+            <br>
+            Why you want this job: <input type="text" name="application_reason">
+            <br>
+            <br>
+            <button type="submit">Submit Application</button>
+        </form>
+    </body>
+</html>
