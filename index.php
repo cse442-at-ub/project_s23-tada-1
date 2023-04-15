ea966dd0d16b44a61e7e9e0f9f529715c5f4291d
Tim <tkong@buffalo.edu>
2023-04-15 18:52:06 -0400

    fixed naming for job application database #51

--- index.php
+++ index.php
@@ -74,7 +74,7 @@ console_log("Running on " . php_sapi_name());
 		$conn = mysqli_connect("oceanus.cse.buffalo.edu:3306", "khlam", "50338576", "cse442_2023_spring_team_p_db");
 
 
-		$query = "INSERT INTO jobApplications(name, experience, reason) VALUES ($app_name, $app_experience, $app_reason)";
+		$query = "INSERT INTO jobApplications(Name, Experience, Reason) VALUES ($app_name, $app_experience, $app_reason)";
 
 		mysqli_query($conn, $query);
 		mysqli_close($conn);
