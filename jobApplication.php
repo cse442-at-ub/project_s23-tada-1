ef0e51f2185c154efcbb8bf54305617ce6b8761e
Tim <tkong@buffalo.edu>
2023-04-15 17:35:20 -0400

    connecting the job board to job application and adding entries to the database #51

--- jobApplication.php
+++ jobApplication.php
@@ -1,18 +1,66 @@
+<?php
+require('./backend/head.php');
+?>
 <!DOCTYPE html>
 <html>
-    <head></head>
+    <head><<?php head("Job Application")?>/head>
     <body>
+
+        <?php
+            require("./backend/log.php");
+            require('./backend/connection.php');
+
+            if ($_SERVER["REQUEST_METHOD"] === "GET") {
+                if (isset($_GET["id"])) {
+                    console_log($_GET["id"]);
+                    // echo $_GET["id"];
+                    // echo urldecode($_GET["id"]);
+                    // echo $_GET["table"];
+                }
+            }
+            // echo "Job Application Details: ";
+            // echo $_GET["id"];
+
+            $conn = mysqli_connect("oceanus.cse.buffalo.edu:3306", "khlam", "50338576", "cse442_2023_spring_team_p_db");
+            
+            $job_id = urldecode($_GET["id"]);
+            // $job_serve = $conn->prepare("SELECT $job_id FROM `Jobs`");
+            $query = "SELECT $job_id FROM `Jobs`";
+            $job_serve = mysqli_query($conn, $query);
+            $job_info = mysqli_fetch_array($job_serve, MYSQLI_ASSOC);
+
+            $job_title = $job_info["Title"];
+            $job_professor = $job_info["Professor"];
+            $job_description = $job_info["Description"];
+
+
+            echo "Title: ";
+            echo $job_title;
+            echo "<br>";
+
+            echo "Employer: ";
+            echo $job_professor;
+            echo "<br>";
+
+            echo "Description: ";
+            echo $job_description;
+            echo "<br>";
+            echo "<br>";
+        ?>
+
         <form method="GET" action="index.php">
             Name: <input type="text" name="application_name">
             <br>
             <br>
-            Experience: <input type="text" name="application_experience">
+            Experience: <br>
+            <input type="text" style="height:140px; width:300px" name="application_experience">
             <br>
             <br>
-            Why you want this job: <input type="text" name="application_reason">
+            Why you want this job: <br>
+            <input type="text" style="height:140px; width:300px" name="application_reason">
             <br>
             <br>
-            <button type="submit">Submit Application</button>
+            <button type="submit" class="base-button green-button">Submit Application</button>
         </form>
     </body>
 </html>
