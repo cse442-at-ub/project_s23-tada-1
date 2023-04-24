cff4e8a0d0f52207833b8e51d0026330264a3355
srcalo <smilingcalo@gmail.com>
2023-04-24 00:22:44 -0400

    Created form and database call for job creator #56
    - Created submission form for new jobs
    - Wrote database call to submit new jobs

--- backend/user.php
+++ backend/user.php
@@ -52,9 +52,10 @@ function clean_data($data)
     return $data;
 }
 
-
-// Takes in a username
-// Returns true if the user is a professor, else false
+/*
+    Takes in a username
+    Returns true if the user is a professor, else false
+*/
 function isProfessor($username)
 {
     require('backend/connection.php');
@@ -63,11 +64,14 @@ function isProfessor($username)
     $statement->execute();
     $result = $statement->get_result();
     $row = $result->fetch_assoc();
-    return $row["Type"] == "Professor";
+    $isProf = $row["Type"] == "Professor";
+    return $isProf;
 }
 
-// Takes in the page to redirect to in case user is not logged in
-// If user is not logged in, page will redirect. Else do nothing
+/*
+    Takes in the page to redirect to in case user is not logged in
+    If user is not logged in, page will redirect. Else do nothing
+*/
 function isLoggedIn($username, $redirect)
 {
     if ($username == "") {    // If user isn't logged in go back to home page
---
+++ database/createJob.php
@@ -0,0 +1,16 @@
+<?php
+require('../backend/log.php');
+require('../backend/session.php');
+require('../backend/connection.php');
+
+$username = startSession();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    error_log(print_r($_POST, true));
+
+    $statement = $conn->prepare("INSERT INTO Jobs (`Title`, `Professor`, `Description`) VALUES (?, ?, ?)");
+    $statement->bind_param('sss', $_POST["Title"], $username, $_POST["Description"]);
+    $statement->execute();
+
+    echo '<meta http-equiv="refresh" content="0; URL=../applications.php">';
+}
