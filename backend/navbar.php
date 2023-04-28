37f30d1d79000e566b1a83987b54ca96a10c0663
Kelly Lam <khlam@buffalo.edu>
2023-04-28 17:09:23 -0400

    Made functionality to the navbar.php and made a logout.php file #62
    Made the links for the navbar (profile, my schedule edit etc.) and made a helper file to logout the user by deleting the session.

--- backend/navbar.php
+++ backend/navbar.php
@@ -13,10 +13,26 @@ function navbar($username)
 				</ul>
 			</div>
 			<div class="menu-container">
-				<ul class="nav>
-					<li> 
-						<p>link here etc.</p>
+				<ul class="nav">
+					<li>
+						<a href="profile.php">Profile</a>
 					</li>
+					<li> 
+						<a href="mySchedule.php">My Schedule</a>
+					</li>
+					<li> 
+						<a href="jobBoard.php">Job Board</a>
+					</li>
+					<li> 
+						<a href="editSchedule.php">Edit Schedule</a>
+					</li>
+					<li> 
+						<a href="jobCreator.php">Job Creator</a>
+					</li>
+					<li>
+						<a href="/backend/logout.php">Logout</a> 
+					</li>
+
 				</ul>
 			</div>
 		</div>
